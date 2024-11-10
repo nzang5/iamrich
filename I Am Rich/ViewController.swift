6337adc6107f56df00bd124eb0069c87392611a7
Nash Zangio <nash.zangio@qonto.com>
2024-11-10 20:22:59 +0100

    completed

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Nash Zangio on 10.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
